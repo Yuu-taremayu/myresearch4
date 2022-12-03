5330cb582ad88895ba1be70f450caf742a3eb6fc
Yuu-taremayu <taremayu.yuu@gmail.com>
2022-12-04 04:08:28 +0900

    add css build rule

--- programs/treeUI/build.sh
+++ programs/treeUI/build.sh
@@ -1,10 +1,14 @@
 SRCDIR="./src/js/"
 DSTDIR="./build/"
+CSSDIR="./src/css/"
 
-# Transpile
+# Transpile JS
 babel --presets react,es2015 ${SRCDIR} -d ${DSTDIR}
 
-# Packaging
+# Packaging JS
 browserify ${DSTDIR}index.js -o bundle.js
+
+#Packaging CSS
+cat ${CSSDIR}* > bundle.css
 
 date; echo;
