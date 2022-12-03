d8f84a3f13b78a5f2c0b189b316631285db861bb
Yuu-taremayu <taremayu.yuu@gmail.com>
2022-12-04 02:37:29 +0900

    testing

---
+++ programs/treeUI/build/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _client = require('react-dom/client');
+
+var ReactDOM = _interopRequireWildcard(_client);
+
+var _App = require('./App');
+
+var _App2 = _interopRequireDefault(_App);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var root = ReactDOM.createRoot(document.getElementById('root'));
+//const element = <p>a</p>;
+/*
+const element = React.createElement(
+  'h1',
+    {className: 'greeting'},
+      'Hello, world!'
+      );
+*/
+root.render(_react2.default.createElement(_App2.default, null));
